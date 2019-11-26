8d8b4f8a55ab3a8d9ed4f694fc701973d9400955
DuPengFei-du <935820240@qq.com>
2019-11-26 10:21:25 +0800

    math

---
+++ 二分查找/二分查找/二分查找.c
@@ -0,0 +1,48 @@
+/*
+对于一个有序数组，我们通常采用二分查找的方式来定位某一元素，
+请编写二分查找的算法，在数组中查找指定元素。
+给定一个整数数组A及它的大小n，同时给定要查找的元素val，
+请返回它在数组中的位置(从0开始)，若不存在该元素，返回-1。
+若该元素出现多次，请返回第一次出现的位置。
+
+测试样例：
+[1,3,5,7,9],5,3
+返回：1
+
+*/
+
+
+
+
+//class BinarySearch {
+//public:
+//	int getPos(vector<int> A, int n, int val)
+//	{
+//		// write code here
+//		int left = 0;
+//		int right = n - 1;
+//		while (left <= right)
+//		{
+//			int mid = left + (right - left) / 2;
+//			if (val == A[mid])
+//			{
+//				while (val == A[mid])
+//				{
+//					mid--;
+//				}
+//				return mid + 1;
+//			}
+//			if (val < A[mid])
+//			{
+//				right = mid - 1;
+//			}
+//			else if (val > A[mid])
+//			{
+//				left = mid + 1;
+//			}
+//			else
+//				return mid;
+//		}
+//		return -1;
+//	}
+//};
