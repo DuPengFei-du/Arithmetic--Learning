2d50b54c2dd530b29e4d25c3c40fd31bf57f5edd
DuPengFei-du <935820240@qq.com>
2020-01-23 12:22:14 +0800

    test

---
+++ Strlen模拟实现/Strlen模拟实现/main.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//递归
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	if (*str == '\0')
+		return 0;
+	else
+		return 1 + my_strlen(str + 1);
+}
+int main()
+{
+	char arr[] = "Hello World";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
+
+
+
+//计数器
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+int main()
+{
+	char arr[] = "Hello World";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
+
+
+//利用指针-指针的方法
+//得出两个指针之间的元素个数
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	char* start = str;
+	while (*str != '\0')
+	{
+		str++;
+	}
+	return str - start;
+}
+int main()
+{
+	char arr[] = "Hello World";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
