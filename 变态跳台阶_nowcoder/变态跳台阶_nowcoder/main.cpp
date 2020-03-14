5cde418ef266bc41cabe45d6c5b16045df3e0f4b
DuPengFei-du <935820240@qq.com>
2020-03-14 14:16:55 +0800

    math

---
+++ 变态跳台阶_nowcoder/变态跳台阶_nowcoder/main.cpp
@@ -0,0 +1,19 @@
+/*
+题目描述
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+*/
+
+
+//代码如下所示：
+class Solution {
+public:
+	int jumpFloorII(int number)
+	{
+		if (number == 1)
+			return 1;
+		else if (number == 2)
+			return 2;
+		else
+			return 2 * jumpFloorII(number - 1);
+	}
+};
